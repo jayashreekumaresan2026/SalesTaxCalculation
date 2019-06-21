41617a4728bf6eb6bc5804f61257d8854d581267
Jayashree <jayashree.kammu@gmail.com>
2019-06-21 10:41:21 +0530

    get input and parse the input

--- src/com/company/Application.java
+++ src/com/company/Application.java
@@ -1,11 +1,13 @@
 package com.company;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         List<Item> items = new ArrayList<>();
+        List<Item> parseItems = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of items :");
         int itemCount = Integer.parseInt(scanner.nextLine());
@@ -22,5 +24,22 @@ public class Application {
         for (Item item : items) {
             item.showName();
         }
+        System.out.print("Enter the String :");
+        itemCount = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < itemCount; i++) {
+            System.out.print("Enter the input string :");
+            String sentence = scanner.nextLine();
+            String[] words = sentence.split(" ");
+            Arrays.asList(words);
+            int quantity = Integer.parseInt(words[0]);
+            String itemName = words[1];
+            double price = Double.parseDouble(words[3]);
+            Item item = new Item(itemName, price, quantity);
+            parseItems.add(item);
+        }
+        for (Item item : parseItems) {
+            System.out.println(item.toString());
+        }
+        System.out.println("---------------------------------");
     }
 }
