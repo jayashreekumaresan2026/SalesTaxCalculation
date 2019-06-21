2c805252524561cf5e64dcc6b19119041c19fcbd
Jayashree <jayashree.kammu@gmail.com>
2019-06-21 11:03:18 +0530

    get excluded tax items

--- src/com/company/Items.java
+++ src/com/company/Items.java
@@ -1,13 +1,17 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Items {
     static double TaxAmount = 0.15;
     private List<Item> items;
+    private List<String> exemptedSalesTaxItems;
 
     Items(List<Item> items) {
         this.items = items;
+        this.exemptedSalesTaxItems = new ArrayList<>(Arrays.asList("pen", "pencil", "scale", "eraser"));
     }
     double calculateSalesTax() {
         double taxCost = 0;
@@ -16,6 +20,9 @@ public class Items {
             item.showCalculation(taxCost);
         }
         return taxCost;
+    }
+    List<String> getExemptedSalesTaxItems() {
+        return exemptedSalesTaxItems;
     }
 
 }
